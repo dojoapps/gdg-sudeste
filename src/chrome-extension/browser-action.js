f78fb70af63b6218cb6cb1ed22edc7a56d34d621
Felipe Amorim <felipe.leusin@gmail.com>
2014-11-08 12:03:32 -0200

    buscando linguagens

--- src/chrome-extension/browser-action.js
+++ src/chrome-extension/browser-action.js
@@ -1,17 +1,42 @@
 Parse.initialize("02iUkCJOt8dlrS6AxmNlgrLh5qy35eyWiRzD9dkm", "xC6fqYNjCZEpLh4ybBrWGhzJAyPmXvGsqJGqMlVg");
 
 var user = window.localStorage["user"];
+var languageActions = {};
 
-var TestObject = Parse.Object.extend("TestObject");
-var testObject = new TestObject();
-testObject.save({foo: "bar"}).then(function(object) {
-  alert("yay! it worked");
-});
-
-console.log(user);
+var ParseLanguage = Parse.Object.extend("ParseLanguage");
+var ParseTranslation = Parse.Object.extend("ParseTranslation");
 
 function loadLanguages() {
+  if (!user) {
+    return;
+  }
 
+  var query = new Parse.Query("ParseLanguage");
+  query.equalTo("user", user);
+  query.ascending("name");
+  query.find({
+    success: function (results) {
+      for (var i = 0, l = results.length; i < 0; i++) {
+        var language = results[i];
+        if (!languageActions[language.code]) {
+          languageActions[language.code] = chrome.contextMenus.create({
+            title: "Traduzir para " + language.name,
+            contexts: ["all"],
+            onclick: handleTranslateTo(language.code)
+          });
+        }
+      };
+    },
+    error: function (error) {
+      console.log(error);
+    }
+  });
+}
+
+function handleTranslateTo(languageCode) {
+  return function (info, tab) {
+    //TODO: Salvar a ParseTranslation e enviar o Push
+  };
 }
 
 chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
